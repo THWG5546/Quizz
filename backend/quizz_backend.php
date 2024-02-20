a9d05190d1bf515e1a885db098f3d28c963b22bf
Allan <allanbecker@efrei.net>
2024-02-20 12:05:49 +0100

    modiff quizz

--- backend/quizz_backend.php
+++ backend/quizz_backend.php
@@ -1,15 +1,23 @@
 <?php
 include 'db_connect.php';
 
-$id = $_GET['id'];
-$sql = "SELECT * FROM quizz WHERE id = $id";
-$result = $conn->query($sql);
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM quizz WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    echo json_encode($row);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        echo json_encode($row);
+    } else {
+        echo json_encode((object) array());
+    }
+    $stmt->close();
 } else {
-    echo json_encode((object) array());
+    echo "ID Inconnu";
 }
 
 $conn->close();
