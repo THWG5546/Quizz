a992e0fba19ffef6f74f79eb8bd6b8fc046bade2
Allan <allanbecker@efrei.net>
2024-02-29 01:12:25 +0100

    bgubnk

--- backend/submit_fairequizz.php
+++ backend/submit_fairequizz.php
@@ -2,27 +2,27 @@
 header("Access-Control-Allow-Origin: https://thwg5546.github.io");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include 'db_connect.php';
-    // Collecte des data du form
-    $nom = htmlspecialchars($_REQUEST['quiz-name']);
-    $prenom = htmlspecialchars($_REQUEST['quiz-firstname']);
-    $idquizz = htmlspecialchars($_REQUEST['quiz-id']);
-    $idquest = 1;
-    if (empty($nom) || empty($prenom) || empty($idquizz)) {
-        echo "Tous les éléments sont requis !";
+//if ($_SERVER["REQUEST_METHOD"] == "POST") {
+include 'db_connect.php';
+// Collecte des data du form
+$nom = htmlspecialchars($_REQUEST['quiz-name']);
+$prenom = htmlspecialchars($_REQUEST['quiz-firstname']);
+$idquizz = htmlspecialchars($_REQUEST['quiz-id']);
+$idquest = 1;
+if (empty($nom) || empty($prenom) || empty($idquizz)) {
+    echo "Tous les éléments sont requis !";
+} else {
+    $sql = "INSERT INTO users (nom, prenom) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $nom, $prenom);
+
+    if ($stmt->execute()) {
+        //header("Location:https://thwg5546.github.io/Quizz/testquizz.html?idquizz=" . $idquizz . "?id=" . $idquest);
     } else {
-        $sql = "INSERT INTO users (nom, prenom) VALUES (?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ss", $nom, $prenom);
-
-        if ($stmt->execute()) {
-            //header("Location:https://thwg5546.github.io/Quizz/testquizz.html?idquizz=" . $idquizz . "?id=" . $idquest);
-        } else {
-            echo "Error: " . $stmt->error;
-        }
-
-        $stmt->close();
-        $conn->close();
+        echo "Error: " . $stmt->error;
     }
+
+    $stmt->close();
+    $conn->close();
 }
+//}
