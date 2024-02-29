2ff8fa8641ab491538c2657f86b51a0794168ed2
Allan <allanbecker@efrei.net>
2024-02-29 02:13:02 +0100

    efefds

--- IDquizz.js
+++ IDquizz.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         console.log("Le bouton de validation a été cliqué !");
         localStorage.setItem('quizzindex', idquizz);
         localStorage.setItem('questionIndex', 1);
-        fetch('http://localhost/backend/submit_fairequizz.php?idquizz=' + idquizz + '?id=' + questionIndex)
+        fetch('http://localhost/backend/submit_fairequizz.php?idquizz=' + idquizz + '?id=' + 1)
         let url = 'https://thwg5546.github.io/Quizz/testquizz.html?idquizz=' + idquizz + "?id=" + 1;
         window.location.href = url;
     });
