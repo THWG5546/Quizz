d3adf2c0b7c9ca55e9772ce34b741b87b5cbd510
Sam <153606332+Sammixi@users.noreply.github.com>
2024-02-29 01:11:22 +0100

    c chiant

--- backend/submit_note.php
+++ backend/submit_note.php
@@ -1,23 +1,23 @@
 <?php
-include 'db_connect.php';
+include 'db_connect.php'; 
 
-if ($conn->connect_error) {
-    die("La connexion a échoué : " . $conn->connect_error);
-}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Vérification de la connexion à la base de données.
+    if ($conn->connect_error) {
+        die("La connexion a échoué : " . $conn->connect_error);
+    }
 
-if (isset($_POST['quizz-id'])) {
-    $quizzId = $_POST['quizz-id'];
+    // Assurez-vous que le nom du champ est correct et correspond à celui du formulaire HTML.
+    $quizzId = isset($_POST['quizz-id']) ? $_POST['quizz-id'] : '';
 
-    $stmt = $conn->prepare("SELECT Note FROM NoteQuizz WHERE ID = ?
-    ");
+    $stmt = $conn->prepare("SELECT Note FROM NoteQuizz WHERE ID = ?");
     $stmt->bind_param("i", $quizzId);
 
     if ($stmt->execute()) {
         $result = $stmt->get_result();
         if ($result->num_rows > 0) {
             $row = $result->fetch_assoc();
-            $note = $row["Note"];
-            echo $note;
+            echo $row["Note"];
         } else {
             echo "ID de quizz non trouvé";
         }
@@ -26,10 +26,8 @@ if (isset($_POST['quizz-id'])) {
     }
 
     $stmt->close();
+    $conn->close();
 } else {
-    echo "Aucun ID de quizz fourni";
-
-    
+    echo "Méthode non autorisée";
 }
-
-$conn->close();
+?>
