b1f039c2c2ae5d8d2c5cbc408d18ce55c5a8c2d9
Allan <allanbecker@efrei.net>
2024-02-29 02:42:30 +0100

    fezezfd

--- IDquizz.js
+++ IDquizz.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     const idquizz = document.getElementById('quiz-id');
+    console.log(idquizz);
     document.getElementById('valider-quizz-btn').addEventListener('click', function () {
         console.log("Le bouton de validation a été cliqué !");
         localStorage.setItem('quizzindex', idquizz);
         localStorage.setItem('questionIndex', 1);
         fetch('http://localhost/backend/submit_fairequizz.php?idquizz=' + idquizz + '?id=' + 1)
-        let url = 'https://thwg5546.github.io/Quizz/testquizz.html?idquizz=' + idquizz + "?id=" + 1;
-        window.location.href = url;
+        //let url = 'https://thwg5546.github.io/Quizz/testquizz.html?idquizz=' + idquizz + "?id=" + 1;
+        //window.location.href = url;
     });
 });
