4e4bf51fdd786f168c63e96277a3437f0583e322
Allan <allanbecker@efrei.net>
2024-02-29 03:20:47 +0100

    rgfdgrdhd

--- IDquizz.js
+++ IDquizz.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     document.getElementById('valider-quizz-btn').addEventListener('click', function () {
         console.log("Le bouton de validation a été cliqué !");
         localStorage.setItem('quizzindex', idquizzValue);
+        console.log(localStorage.getItem('quizzindex'));
         localStorage.setItem('questionIndex', 1);
         fetch('http://localhost/backend/submit_fairequizz.php?idquizz=' + idquizzValue + '?id=' + 1)
         //let url = 'https://thwg5546.github.io/Quizz/testquizz.html?idquizz=' + idquizz + "?id=" + 1;
