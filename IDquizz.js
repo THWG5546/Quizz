9cb96553cb90d155386c082e16ea182a24ca5b2c
Allan <allanbecker@efrei.net>
2024-02-29 02:59:55 +0100

    vkjbh

--- IDquizz.js
+++ IDquizz.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     const idquizz = document.getElementById('quiz-id');
-    console.log(idquizz);
+    var idquizzValue = idquizz.value;
+    console.log(idquizzValue);
     document.getElementById('valider-quizz-btn').addEventListener('click', function () {
         console.log("Le bouton de validation a été cliqué !");
-        localStorage.setItem('quizzindex', idquizz);
+        localStorage.setItem('quizzindex', idquizzValue);
         localStorage.setItem('questionIndex', 1);
-        fetch('http://localhost/backend/submit_fairequizz.php?idquizz=' + idquizz + '?id=' + 1)
+        fetch('http://localhost/backend/submit_fairequizz.php?idquizz=' + idquizzValue + '?id=' + 1)
         //let url = 'https://thwg5546.github.io/Quizz/testquizz.html?idquizz=' + idquizz + "?id=" + 1;
         //window.location.href = url;
     });
