d7fd2eb0924c16fd4a34cf581be79d40c687b9a2
Allan <allanbecker@efrei.net>
2024-02-29 01:46:19 +0100

    uvulyvi

--- backend/submit_fairequizz.php
+++ backend/submit_fairequizz.php
@@ -5,24 +5,23 @@ header("Access-Control-Allow-Headers: Content-Type");
 //if ($_SERVER["REQUEST_METHOD"] == "POST") {
 include 'db_connect.php';
 // Collecte des data du form
-$nom = htmlspecialchars($_REQUEST['quiz-name']);
-$prenom = htmlspecialchars($_REQUEST['quiz-firstname']);
-$idquizz = htmlspecialchars($_REQUEST['quiz-id']);
+$nom = $_GET['quiz-name'];
+$prenom = $_GET['quiz-firstname'];
+$idquizz = $_GET['quiz-id'];
 $idquest = 1;
 if (empty($nom) || empty($prenom) || empty($idquizz)) {
     echo "Tous les éléments sont requis !";
 } else {
-    $sql = "INSERT INTO users (nom, prenom) VALUES (?, ?)";
+    $sql = "INSERT INTO users (nom, prenom, idquizz) VALUES (?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $nom, $prenom);
-
-    if ($stmt->execute()) {
-        //header("Location:https://thwg5546.github.io/Quizz/testquizz.html?idquizz=" . $idquizz . "?id=" . $idquest);
+    $stmt->bind_param("sss", $nom, $prenom, $idquizz);
+    $stmt->execute();
+    /*if ($stmt->execute()) {
+        header("Location:https://thwg5546.github.io/Quizz/testquizz.html?idquizz=" . $idquizz . "?id=" . $idquest);
     } else {
         echo "Error: " . $stmt->error;
-    }
+    }*/
 
     $stmt->close();
     $conn->close();
 }
-//}
